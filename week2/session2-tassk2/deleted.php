f25a9571caca30cae3d70504dec739daad3a1fac
mahmoud-saber <60474033+mahmoud-saber@users.noreply.github.com>
2022-03-15 00:15:02 +0200

    update

---
+++ week2/session2-tassk2/deleted.php
@@ -0,0 +1,16 @@
+<?php
+require 'dbconnect.php';
+
+   if(isset($_GET['id'])){
+    $sql = "DELETE FROM `info` WHERE `id`='$id'";
+    $quary = mysqli_query($conn,$sql); 
+
+   }
+    if(!$quary){
+        // echo 'Raw Inserted';
+        echo 'Error Try Again '.mysqli_error($conn);
+
+    }
+    mysqli_close($conn);
+
+?>
---
+++ week2/session2-tassk2/display.php
@@ -0,0 +1,113 @@
+<?php 
+   
+   require 'dbconnect.php';
+   
+   $sql = "select id , title , content, image from info";
+   
+   $data = mysqli_query($conn,$sql); 
+   
+   
+   ?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PDO - Read Records - PHP CRUD Tutorial</title>
+
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+
+    <!-- custom css -->
+    <style>
+    .m-r-1em {
+        margin-right: 1em;
+    }
+
+    .m-b-1em {
+        margin-bottom: 1em;
+    }
+
+    .m-l-1em {
+        margin-left: 1em;
+    }
+
+    .mt0 {
+        margin-top: 0;
+    }
+    </style>
+
+</head>
+
+<body>
+
+    <!-- container -->
+    <div class="container">
+
+
+        <div class="page-header">
+            <h1>Read Users </h1>
+            <br>
+
+
+
+        </div>
+
+        <a href="">+ Account</a>
+
+        <table class='table table-hover table-responsive table-bordered'>
+            <!-- creating our table heading -->
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Content</th>
+                <th>Image</th>
+                <th>action</th>
+            </tr>
+
+
+
+            <?php 
+       
+            while($raw = mysqli_fetch_assoc($data)){
+   
+       ?>
+
+            <tr>
+
+                <td><?php echo $raw['id'];?></td>
+                <td><?php echo $raw['title'];?></td>
+                <td><?php echo $raw['content'];?></td>
+                <td> <img src="<?php echo $raw['image'];?>" alt="" width="100px"></td>
+                <td>
+                    <a href='deleted.php' class=' btn btn-danger m-r-1em'>Delete</a>
+                </td>
+
+            </tr>
+            <?php } 
+   
+   
+   
+   mysqli_close($conn);
+   
+   ?>
+
+            <!-- end table -->
+        </table>
+
+    </div>
+    <!-- end .container -->
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+    <!-- Latest compiled and minified Bootstrap JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <!-- confirm delete record will be here -->
+
+</body>
+
+</html>
