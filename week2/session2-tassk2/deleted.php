83b5f789bfbcd7113a2708ed1f9a83db43824a43
mahmoud-saber <60474033+mahmoud-saber@users.noreply.github.com>
2022-03-15 00:21:45 +0200

    update

--- week2/session2-tassk2/deleted.php
+++ week2/session2-tassk2/deleted.php
@@ -5,12 +5,12 @@ require 'dbconnect.php';
     $sql = "DELETE FROM `info` WHERE `id`='$id'";
     $quary = mysqli_query($conn,$sql); 
 
-   }
+   
     if(!$quary){
         // echo 'Raw Inserted';
         echo 'Error Try Again '.mysqli_error($conn);
 
     }
     mysqli_close($conn);
-
+   }
 ?>
